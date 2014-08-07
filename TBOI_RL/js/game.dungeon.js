4bbb8a9e45ac4058ea304197ca3efca4dea143ff
G3 Kappa <g3kappa@gmail.com>
2014-08-07 17:07:48 +0200

    Implemented movement and boundary checks

--- TBOI_RL/js/game.dungeon.js
+++ TBOI_RL/js/game.dungeon.js
@@ -5,29 +5,21 @@ function Dungeon(drawx, drawy, width, height) {
 	this.width = width || 256;
 	this.height = height || 160;
 
-
-	var DOOR = {
-		NONE : 'none',
-		NORMAL : 'map_doornormal_',
-	};
-
 	var rooms = [];
-	var doors = {
-		left: DOOR.NORMAL,
-		right: DOOR.NORMAL,
-		up: DOOR.NORMAL,
-		down: DOOR.NORMAL	
-	};
 	var current_room; //Only one room at a time is updated and drawn, obviously
 	var bound_entity; //The entity which will receive input from the player
 	var bound_entity_defupdate; //The default update method for a bound entity. Overridden by manual input and later restored.
 
 	var override_update = function(delta) {
-
+		bound_entity.applyForce(0, 0, delta);
+		if(keydown.w) bound_entity.applyForce(0, -1, delta);
+		if(keydown.a) bound_entity.applyForce(-1, 0, delta);
+		if(keydown.s) bound_entity.applyForce(0, 1, delta);
+		if(keydown.d) bound_entity.applyForce(1, 0, delta);
 	};
 
 	//Called whenever the game must inform the player of something
-	//The suggested use would be to display output directly on the page rather than in the console window
+	//It should be overridden
 	this.log = function(message) { 
 		console.log(message); 
 	};
@@ -48,11 +40,14 @@ function Dungeon(drawx, drawy, width, height) {
 		this.log('Generating dungeon...');
 		type = type || 'basement';
 		//TODO: Actually add generation code
-		switch(type) {
+		switch(type) {	
 			case 'basement':
 			default:
 				current_room = new Room('bg_basement', this.x, this.y);
-				current_room.draw_overlay = true;	
+				current_room.draw_overlay = true;
+				bound_entity.x = this.x + this.width / 2 - bound_entity.sprite.width / 2;
+				bound_entity.y = this.y + this.height / 2 - bound_entity.sprite.height / 2;
+				current_room.entities.push(bound_entity);	
 			break;
 		};
 		this.log('&nbsp;&nbsp;&nbsp;&nbsp;... Done.');
@@ -64,41 +59,42 @@ function Dungeon(drawx, drawy, width, height) {
 
 	this.draw = function(canvas) {
 		current_room.draw(canvas);
-		//Draw the doors
-		if(doors.left != DOOR.NONE) resources[doors.left + 'left'].draw(canvas, 0, this.y + this.height / 2 - resources[doors.left + 'left'].height / 2);
-		if(doors.right != DOOR.NONE) resources[doors.right + 'right'].draw(canvas, this.x + this.width - resources[doors.right + 'right'].width, this.y + this.height / 2 - resources[doors.right + 'right'].height / 2);
-		if(doors.up != DOOR.NONE) resources[doors.up + 'up'].draw(canvas, this.x + this.width / 2 - resources[doors.up + 'up'].width / 2, 0);
-		if(doors.down != DOOR.NONE)  resources[doors.down + 'down'].draw(canvas, this.x + this.width / 2 - resources[doors.down + 'down'].width / 2, this.y + this.height - resources[doors.down + 'down'].height) ;
 	};
 
 };
 
 function Room(room_bg, drawx, drawy) {
+	Room.BOUNDARIES = {
+		x: 16,
+		y: 16,
+		x2: 240,
+		y2: 144
+	};
+
 	var room_bg = room_bg || 'bg_basement';
 	var drawx = drawx || 0;
 	var drawy = drawy || 0;
 
 	this.draw_overlay = false;
 	this.entities = [];
-	this.items = [];
 
 	this.update = function(delta) {
-		$.each(this.entities, function(index) {
-			this.entities[i].update(delta);
-		});
-		$.each(this.items, function(index) {
-			this.items[i].update(delta);
+		$.each(this.entities, function(i, e) {
+			e.update(delta);
+			//Making sure each entity stays within the boundaries
+			//Collisions are handled by the Room so that entities themselves stay simple
+			if(e.x < Room.BOUNDARIES.x) e.x = Room.BOUNDARIES.x;
+			else if(e.x + e.sprite.width > Room.BOUNDARIES.x2) e.x = Room.BOUNDARIES.x2 - e.sprite.width;
+			if(e.y < Room.BOUNDARIES.y) e.y = Room.BOUNDARIES.y;
+			else if(e.y + e.sprite.height > Room.BOUNDARIES.y2) e.y = Room.BOUNDARIES.y2 - e.sprite.height;
 		});
 	};
 
 	this.draw = function(canvas) {
 		resources[room_bg].draw(canvas, drawx, drawy);
 		if(this.draw_overlay) resources['bg_intro_overlay'].draw(canvas, drawx, drawy);
-		$.each(this.entities, function(index) {
-			this.entities[i].draw(delta);
-		});
-		$.each(this.items, function(index) {
-			this.items[i].draw(delta);
+		$.each(this.entities, function(i, e) {
+			e.draw(canvas);
 		});
 	};
 };
--- TBOI_RL/js/game.entity.js
+++ TBOI_RL/js/game.entity.js
@@ -1,29 +1,59 @@
-	function Entity() {
-		this.name = 'Unnamed Entity';
-		this.sprite = resources['ent_sprite_default'];
-		this.info = {
-			health: 4,
-			maxhealth: 4,
-			soulhearts: 0,
-			eternalhearts: 0,
-			coins: '00',
-			keys: '00',
-			bombs: '00'
-		};
-		Entity.MAX_HEALTH = 20;
+function Entity() {
+	Entity.MAX_HEALTH = 20;
+	Entity.FRICTION = 0.90;
 
-		this.update = function(delta) {
-
-		};
-
-		this.draw = function(canvas) {
-
-		};
+	this.name = 'Unnamed Entity';
+	this.sprite = resources['ent_sprite_default'];
+	this.info = {
+		health: 4,
+		maxhealth: 4,
+		soulhearts: 0,
+		eternalhearts: 0,
+		coins: '00',
+		keys: '00',
+		bombs: '00'
+	};
+	this.speed = {
+		x: 0.00033,
+		y: 0.00033
 	};
 
-	function ent_Isaac() {
-		Entity.call(this);
-		this.name = 'Isaac';
-		this.sprite = resources['ent_sprite_isaac'];
+	this.x = 0;
+	this.y = 0;
+
+	//Current velocity
+	var velocity_x = 0;
+	var velocity_y = 0;
+	this.applyForce = function(accel_x, accel_y, dt) {
+		this.x += velocity_x * dt;
+		this.y += velocity_y * dt;
+		velocity_x = Entity.FRICTION * (velocity_x + (this.speed.x * accel_x) * dt);
+		velocity_y = Entity.FRICTION * (velocity_y + (this.speed.y * accel_y) * dt);
 	};
-	ent_Isaac.prototype = new Entity;
+
+	this.onCollision = function(e) {
+
+	};
+
+	this.update = function(delta) {
+
+	};
+
+	this.draw = function(canvas) {
+		this.sprite.draw(canvas, this.x, this.y);
+	};
+};
+//Functional entities (Bombs, rocks, chests...)
+function ent_Bomb() {
+	Entity.call(this);
+	this.name = 'Bomb';
+	this.sprite = resources['ent_sprite_isaac'];
+};
+ent_Bomb.prototype = new Entity;
+//Alive entities (Enemies, playable characters...)
+function ent_Isaac() {
+	Entity.call(this);
+	this.name = 'Isaac';
+	this.sprite = resources['ent_sprite_isaac'];
+};
+ent_Isaac.prototype = new Entity;
